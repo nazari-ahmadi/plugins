81671154504856fa110b1ecfb1ae909b2db19db1
nazari-ahmadi <e.ahmadi67@yahoo.com>
2018-10-28 10:26:44 +0330

    add Weght to sepehr.details

---
+++ sepehr/details/controllers/Weights.php
@@ -0,0 +1,21 @@
+<?php namespace Sepehr\Details\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Weights extends Controller
+{
+    public $implement = ['Backend\Behaviors\ListController', 'Backend\Behaviors\FormController'];
+
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    protected $requiredPermissions = ['sepehr.details.service.access_weight'];
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Sepehr.Details', 'details', 'services');
+
+    }
+}

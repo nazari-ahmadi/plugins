29572786c7136fbced4581368546cb581e459cb2
nazari-ahmadi <e.ahmadi67@yahoo.com>
2018-11-04 13:01:41 +0330

    update Services

--- sepehr/service/controllers/Services.php
+++ sepehr/service/controllers/Services.php
@@ -6,24 +6,27 @@ use ApplicationException;
 
 class Services extends Controller
 {
-  public $implement = [
-    'Backend\Behaviors\ListController',
-    'Backend\Behaviors\FormController'
-  ];
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController'
+    ];
 
-  public $requiredPermissions = ['sepehr.service.access_service'];
-  public $listConfig = 'config_list.yaml';
-  public $formConfig = 'config_form.yaml';
+    public $requiredPermissions = ['sepehr.service.access_service'];
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
 
-  public function __construct()
-  {
-    parent::__construct();
-    BackendMenu::setContext('Sepehr.Service', 'services', 'service');
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Sepehr.Service', 'services', 'service');
+    }
 
 
-  public function formBeforeCreate($model)
-  {
-    $model->manage_id = $this->user->id;
-  }
+    public function formBeforeCreate($model)
+    {
+        $model->manage_id = $this->user->id;
+        foreach ($model->packages as $package) {
+            $package->is_rejected = false;
+        }
+    }
 }
